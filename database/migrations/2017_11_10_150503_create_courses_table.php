75d25eb5a743e8a5f4ec739516bb54c4ee9ae63e
jchunqui <jose.chunqui.v@uni.pe>
2017-12-10 20:37:42 -0500

    fixed migration courses table

--- database/migrations/2017_11_10_150503_create_courses_table.php
+++ database/migrations/2017_11_10_150503_create_courses_table.php
@@ -16,7 +16,7 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->string('code');
+            $table->string('code')->unique();
             $table->string('name');
             $table->timestamps();
         });
